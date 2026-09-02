59ed47593a1549f5085bdab37c34dd86b606007f
lflondonoc <141975170+lflondonoc@users.noreply.github.com>
2026-09-02 11:17:50 -0500

    unidad 1

---
+++ unidad1/src/Ejemplo.java
@@ -0,0 +1,13 @@
+public class Ejemplo {
+
+    //Método principal (main)
+    void main(){
+        //Función que permite imprimir en pantalla
+        System.out.print("Hola, chicos. Bienvenidos!!! ");
+
+    }
+
+
+}
+
+

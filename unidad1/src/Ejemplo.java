e3054ca5fb4b61252a8ada0dee7ad2df6d0d122b
lflondonoc <141975170+lflondonoc@users.noreply.github.com>
2026-09-04 09:01:07 -0500

    tipos de datos

--- unidad1/src/Ejemplo.java
+++ unidad1/src/Ejemplo.java
@@ -2,6 +2,7 @@ public class Ejemplo {
 
     //Método principal (main)
     void main(){
+
         //Función que permite imprimir en pantalla
         System.out.print("Hola, chicos. Bienvenidos!!! ");
 
--- unidad1/src/TiposDatos.java
+++ unidad1/src/TiposDatos.java
@@ -2,6 +2,22 @@ public class TiposDatos {
 
     void main(){
 
+        //Tipos de datos en java
+        String nombre= "Maria Alejandra";
+        String numeroIdentificacion= "123456";
+        int edad= 18;
+        double estatura= 1.68;
+        String correo= "maria@uqvirtual.edu.co";
+        boolean tieneNovio= false;
+
+        //Función para imprimir
+        // Mi nombres es Maria Alejandra, mi número de identificación es 123456, tengo 18 años, mido 1.68, mi correo electrónico es: maria@uqvirtual.edu.co y ¿tengo novio? false
+
+        System.out.println("Mi nombre es "+nombre+", mi número de identificación es "+numeroIdentificacion+", tengo "+edad+" años, mido "+estatura+", mi correo electrónico es: "+correo+" y ¿tengo novio?: "+tieneNovio);
+
+
+
+
     }
 
 }

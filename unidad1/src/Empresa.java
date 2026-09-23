08f6f6e078df05f4aeb6986bfbb3f85ccc9e780f
lflondonoc <141975170+lflondonoc@users.noreply.github.com>
2026-09-23 10:53:56 -0500

    decisiones

---
+++ unidad1/src/Empresa.java
@@ -0,0 +1,38 @@
+public class Empresa {
+
+    public static final double PORCENTAJE1= 0.10;
+    public static final double PORCENTAJE2= 0.05;
+    public static final double TOPE_SALARIO= 2500000;
+
+    static void main() {
+        double salario= Repositorio.ingresarDecimal("Ingrese su salario: ");
+        double nuevoSalario= calcularAumento(salario);
+        double bonificacion= calcularBonificacion(nuevoSalario);
+        String mensaje= generarMensaje(nuevoSalario, bonificacion);
+        Repositorio.mostrarMensaje(mensaje);
+    }
+
+    public static double calcularAumento (double salario){
+        double nuevoSalario= salario + (salario*PORCENTAJE1);
+        return nuevoSalario;
+    }
+    public static double calcularBonificacion (double nuevoSalario){
+        double bonificacion;
+        if (nuevoSalario > TOPE_SALARIO){
+            bonificacion = nuevoSalario + (nuevoSalario*PORCENTAJE1);
+        }else{
+            bonificacion = nuevoSalario + (nuevoSalario*PORCENTAJE2);
+        }
+        return bonificacion;
+    }
+    public static String generarMensaje (double nuevoSalario, double bonificacion){
+        String mensaje="Su salario con el aumento del 10% es "+nuevoSalario+" Y usted tiene una bonificación del ";
+        if (nuevoSalario > TOPE_SALARIO) {
+            mensaje += "10% por lo tanto su nuevo salario es de: "+bonificacion;
+        }else{
+            mensaje += "5% por lo tanto su nuevo salario es de: "+bonificacion;
+        }
+        return  mensaje;
+    }
+
+}
